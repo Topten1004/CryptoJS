38e8fb135ed32f6389ecc8e9e55577dd967e639d
TopTen1004 <97684719+TopTen1004@users.noreply.github.com>
2023-06-15 11:25:41 +0300

    initial version

---
+++ frontend/src/App.js
@@ -0,0 +1,84 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+import React, { useState } from 'react';
+import CryptoJS from 'crypto-js';
+
+const App = () => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    
+    var key = CryptoJS.enc.Utf8.parse('8056483646328763');
+    var iv = CryptoJS.enc.Utf8.parse('8056483646328763');
+
+    var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(password), key,
+    {
+    keySize: 128 / 8,
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+    }).toString();
+        
+    try {
+
+      console.log(encrypted);
+      const response = await axios.post('https://localhost:7009/User/Login', {
+        userId : userId,
+        password : encrypted,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      // Handle successful login
+      alert("Success");
+    } catch (error) {
+      // Handle login error
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userId">
+            User ID
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="userId"
+            type="text"
+            placeholder="Enter your user ID"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={handleLogin}
+          >
+            Log In
+          </button>
+        </div>
+    </div>
+  );
+};
+
+export default App;
